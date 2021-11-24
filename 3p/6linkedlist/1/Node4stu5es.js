f558d6b598859def075f8aef01a6bb0e4c7b0f07
RobertoABM <90146378+RobertoABM@users.noreply.github.com>
2021-11-24 03:26:22 -0600

    Delete 3p directory

--- 3p/6linkedlist/1/Node4stu5es.js
+++
@@ -1,8 +0,0 @@
-//
-// es5
-//
-
-function Node(data) {//constructor
-   this.data = data
-   this.next = null
-}
--- 3p/6linkedlist/2/traverse.js
+++
@@ -1,7 +0,0 @@
-const traverse=(n)=>{//es6
-    let c=n//iterator 
-    while(c){//exists
-        console.log(c.data)
-	c=c.next
-    }
-}
